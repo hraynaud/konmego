589037e9ce37e9d4c65b8735a6f53bcfaa76858a
Herby Raynaud <herby@yieldmo.com>
2019-04-13 07:47:10 -0400

    cleans up necro code

--- konmego-client/src/main.js
+++ konmego-client/src/main.js
@@ -1,33 +1,9 @@
 import Vue from 'vue'
-// import App from './App.vue'
-// import router from './router'
-// import axios from 'axios'
 import './registerServiceWorker'
-
-
-
 import { router } from './_services';
 import App from './app/App';
 
 Vue.config.productionTip = false
-
-// new Vue({
-//   router,
-//   methods: {
-//     writeToApi: function(payload){
-//       axios.post(url, payload.data, {reponseType: 'json'})
-//       .then(function (response) {
-
-//       })
-//       .catch(function (error) {
-//       })
-//     }
-//   },
-//   render: h => h(App)
-// }).$mount('#app')
-
-
-
 
 new Vue({
     el: '#app',
