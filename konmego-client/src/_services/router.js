3304ac159e015a12604750ebc508d893922dc427
Herby Raynaud <herby@yieldmo.com>
2019-04-20 13:19:29 -0400

    Adds support for error page

--- konmego-client/src/_services/router.js
+++ konmego-client/src/_services/router.js
@@ -1,18 +1,20 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import HomePage from '../home/HomePage'
+import ProjectPage from '../home/ProjectPage'
 import LoginPage from '../login/LoginPage'
+import ErrorPage from '../home/ErrorPage'
 import { authService } from './auth.service'
 
 Vue.use(Router);
-
-
 
 export const router = new Router({
   mode: 'history',
   routes: [
     { path: '/', component: HomePage, props: loggedInUser },
     { path: '/login', component: LoginPage },
+    { path: '/projects', component: ProjectPage, props: project },
+    { path: '/error', name: "error", component: ErrorPage, props: true},
 
     // otherwise redirect to home 
     { path: '*', redirect: '/' }
@@ -25,7 +27,7 @@ function loggedInUser(route){
 
 router.beforeEach((to, from, next) => {
   // redirect to login page if not logged in and trying to access a restricted page
-  const publicPages = ['/login'];
+  const publicPages = ['/login', '/projects','/error'];
   const authRequired = !publicPages.includes(to.path);
 
   if (authRequired && !authService.currentUser()) {
@@ -37,3 +39,7 @@ router.beforeEach((to, from, next) => {
 
   next();
 })
+
+router.onError(function(err){ 
+  router.push({name: 'error', params: {message: err.message}} )
+})    
