0034693f47fc74cc2c927041399f4a69772ee16d
Herby Raynaud <herby@yieldmo.com>
2019-04-21 00:47:44 -0400

    Login failure test passing

--- konmego-client/tests/unit/services/authservice.spec.js
+++ konmego-client/tests/unit/services/authservice.spec.js
@@ -4,22 +4,9 @@ import { authService, __RewireAPI__ as apiServiceRewireApi } from '@/_services/a
 const { localStorage, sessionStorage } = window
 const user = { first: "herby", last: "plerby" }
 
-before(function () {
-  const mockApiService = {
-    writeToApi: function (path, data) {
-      return Promise.resolve({
-          data: {
-            jwt: "eyJhbGciOiJIUzI1NiJ9.eyJ1aWQiOjIsImZpcnN0IjoiSGVyYnkiLCJsYXN0IjoiUmF5bmF1ZCIsImV4cCI6MTU1NTkwMzcyOH0.R8ObE0AMRyhkes7gwjWX2Vd9B0WitUx0fnccJGuSDKI"
-          }
-      })
-    }
-  };
-
-  apiServiceRewireApi.__Rewire__('apiService', mockApiService);
-});
-
-after(function () {
-  apiServiceRewireApi.__ResetDependency__('apiService');
+afterEach(function () {
+  localStorage.clear()
+  sessionStorage.clear()
 })
 
 describe('Auth Service', () => {
@@ -47,17 +34,46 @@ describe('Auth Service', () => {
     })
 
     describe('login', () => {
-      it('should log user in', () => {
 
-       return authService.login("herby", "test")
-          .then(()=>{
+      it('should log user in', () => {
+        const mockApiService = {
+          writeToApi: function (path, data) {
+            return Promise.resolve({
+              data: {
+                jwt: "eyJhbGciOiJIUzI1NiJ9.eyJ1aWQiOjIsImZpcnN0IjoiSGVyYnkiLCJsYXN0IjoiUmF5bmF1ZCIsImV4cCI6MTU1NTkwMzcyOH0.R8ObE0AMRyhkes7gwjWX2Vd9B0WitUx0fnccJGuSDKI"
+              }
+            })
+          }
+        };
+
+        apiServiceRewireApi.__Rewire__('apiService', mockApiService);
+
+        return authService.login("herby", "test")
+          .then(() => {
             expect(sessionStorage.getItem("jwt")).to.not.be.null;
-            expect(sessionStorage.getItem("jwt")).to.not.eql('eyJ1aWQiOjIsImZpcnN0IjoiSGVyYnkiLCJsYXN0IjoiUmF5bmF1ZCIsImV4cCI6MTU1NTkwMzcyOH0');
+            expect(sessionStorage.getItem("jwt")).to.eql('eyJhbGciOiJIUzI1NiJ9.eyJ1aWQiOjIsImZpcnN0IjoiSGVyYnkiLCJsYXN0IjoiUmF5bmF1ZCIsImV4cCI6MTU1NTkwMzcyOH0.R8ObE0AMRyhkes7gwjWX2Vd9B0WitUx0fnccJGuSDKI');
+            apiServiceRewireApi.__ResetDependency__('apiService');
           })
-         
+      });
+
+      it('should fail log user in', () => {
+        const mockApiService = {
+          writeToApi: function (path, data) {
+            return Promise.reject({
+              response: { data: { error: "Wrong credentials bro" } }
+            })
+          }
+        };
+
+        apiServiceRewireApi.__Rewire__('apiService', mockApiService);
+
+        return authService.login("herby", "test")
+          .catch((error) => {
+            expect(sessionStorage.getItem("jwt")).to.be.null
+          })
+
       });
     })
   });
-
 })
 
