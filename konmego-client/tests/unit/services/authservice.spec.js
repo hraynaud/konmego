840fef84886084f9fe8e5c2b0030d72255b2593f
Herby Raynaud <herby@yieldmo.com>
2019-04-21 00:15:04 -0400

    Finally get login/api test passing

--- konmego-client/tests/unit/services/authservice.spec.js
+++ konmego-client/tests/unit/services/authservice.spec.js
@@ -1,48 +1,63 @@
 import { expect } from 'chai'
-import { authService } from '@/_services'
-//import sinon from 'sinon'
+import { authService, __RewireAPI__ as apiServiceRewireApi } from '@/_services/auth.service'
 
 const { localStorage, sessionStorage } = window
 const user = { first: "herby", last: "plerby" }
 
-// sinon.stub(apiService, 'writeToApi').returns(Promise.resolve({
-//   response: {
-//     data: {
-//       jwt: "This is a jwt"
-//     }
-//   }
-// }));
+before(function () {
+  const mockApiService = {
+    writeToApi: function (path, data) {
+      return Promise.resolve({
+          data: {
+            jwt: "eyJhbGciOiJIUzI1NiJ9.eyJ1aWQiOjIsImZpcnN0IjoiSGVyYnkiLCJsYXN0IjoiUmF5bmF1ZCIsImV4cCI6MTU1NTkwMzcyOH0.R8ObE0AMRyhkes7gwjWX2Vd9B0WitUx0fnccJGuSDKI"
+          }
+      })
+    }
+  };
+
+  apiServiceRewireApi.__Rewire__('apiService', mockApiService);
+});
+
+after(function () {
+  apiServiceRewireApi.__ResetDependency__('apiService');
+})
 
 describe('Auth Service', () => {
-  it('has local storage', () => {
-    expect(localStorage).to.not.be.null
-  })
+  it("works ", function () {
 
-  describe("currentUser", () => {
-    it("finds user", () => {
-      localStorage.setItem("user", JSON.stringify(user));
-      expect(authService.currentUser()).to.eql(user);
+    describe("currentUser", () => {
+      it("finds user", () => {
+        localStorage.setItem("user", JSON.stringify(user));
+        expect(authService.currentUser()).to.eql(user);
+      })
     })
-  })
 
-  describe("logout", () => {
-    it("logs user out", () => {
-      localStorage.setItem("user", JSON.stringify(user))
-      sessionStorage.setItem("jwt", "This is encrypted")
-      expect(sessionStorage.getItem("jwt")).to.eql("This is encrypted")
-      expect(JSON.parse(localStorage.getItem("user"))).to.eql(user)
+    describe("logout", () => {
+      it("logs user out", () => {
+        localStorage.setItem("user", JSON.stringify(user))
+        sessionStorage.setItem("jwt", "This is encrypted")
+        expect(sessionStorage.getItem("jwt")).to.eql("This is encrypted")
+        expect(JSON.parse(localStorage.getItem("user"))).to.eql(user)
 
-      authService.logout()
+        authService.logout()
 
-      expect(sessionStorage.getItem("jwt")).to.be.null;
-      expect(localStorage.getItem("user")).to.be.null;
+        expect(sessionStorage.getItem("jwt")).to.be.null;
+        expect(localStorage.getItem("user")).to.be.null;
+      })
     })
-  })
 
-  // describe("login", () => {
+    describe('login', () => {
+      it('should log user in', () => {
 
-  //   authService.login(user)
-  //   expect(sessionStorage.getItem("jwt")).to.not.be.null
-  //   expect(localStorage.getItem("user")).to.not.be.null
-  // })
+       return authService.login("herby", "test")
+          .then(()=>{
+            expect(sessionStorage.getItem("jwt")).to.not.be.null;
+            expect(sessionStorage.getItem("jwt")).to.not.eql('eyJ1aWQiOjIsImZpcnN0IjoiSGVyYnkiLCJsYXN0IjoiUmF5bmF1ZCIsImV4cCI6MTU1NTkwMzcyOH0');
+          })
+         
+      });
+    })
+  });
+
 })
+
