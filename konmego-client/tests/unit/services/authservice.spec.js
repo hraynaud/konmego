e9c25af6c05293b6a0f3fd35427fcca527f59bab
Herby Raynaud <herby@yieldmo.com>
2019-04-21 02:29:29 -0400

    removes “user” magic string

--- konmego-client/tests/unit/services/authservice.spec.js
+++ konmego-client/tests/unit/services/authservice.spec.js
@@ -1,5 +1,5 @@
 import { expect } from 'chai'
-import { authService, SESSION_AUTH_KEY as AUTH_SVC_SESSION_TOKEN_KEY, __RewireAPI__ as apiServiceRewireApi } from '@/_services/auth.service'
+import { authService, SESSION_USER_KEY as AUTH_SVC_SESSION_USER_KEY, SESSION_AUTH_KEY as AUTH_SVC_SESSION_TOKEN_KEY, __RewireAPI__ as apiServiceRewireApi } from '@/_services/auth.service'
 
 const { localStorage, sessionStorage } = window
 const user = { first: "herby", last: "plerby" }
@@ -21,22 +21,22 @@ describe('Auth Service', () => {
 
     describe("currentUser", () => {
       it("finds user", () => {
-        localStorage.setItem("user", JSON.stringify(user));
+        localStorage.setItem(AUTH_SVC_SESSION_USER_KEY, JSON.stringify(user));
         expect(authService.currentUser()).to.eql(user);
       })
     })
 
     describe("logout", () => {
       it("logs user out", () => {
-        localStorage.setItem("user", JSON.stringify(user))
+        localStorage.setItem(AUTH_SVC_SESSION_USER_KEY, JSON.stringify(user))
         sessionStorage.setItem(AUTH_SVC_SESSION_TOKEN_KEY, "This is encrypted")
         expect(sessionStorage.getItem(AUTH_SVC_SESSION_TOKEN_KEY)).to.eql("This is encrypted")
-        expect(JSON.parse(localStorage.getItem("user"))).to.eql(user)
+        expect(JSON.parse(localStorage.getItem(AUTH_SVC_SESSION_USER_KEY))).to.eql(user)
 
         authService.logout()
 
         expect(sessionStorage.getItem(AUTH_SVC_SESSION_TOKEN_KEY)).to.be.null;
-        expect(localStorage.getItem("user")).to.be.null;
+        expect(localStorage.getItem(AUTH_SVC_SESSION_USER_KEY)).to.be.null;
       })
     })
 
