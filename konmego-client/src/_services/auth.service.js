ea70b135b139b0eba775117f321c23d78683c0dd
Herby Raynaud <herby@yieldmo.com>
2019-04-21 02:20:00 -0400

    Cleans up file

--- konmego-client/src/_services/auth.service.js
+++ konmego-client/src/_services/auth.service.js
@@ -1,14 +1,15 @@
 import JWTDecode from 'jwt-decode';
-import { apiService, eventService } from '../_services';
+import { apiService } from '../_services';
+
+const { localStorage, sessionStorage } = window
+export const SESSION_AUTH_KEY = "jwt"
+export const SESSION_USER_KEY = "user"
 
 export const authService = {
     login,
     logout,
-    currentUser,
+    currentUser
 };
-const {localStorage,sessionStorage} = window
-const SESSION_AUTH_KEY = "jwt"
-const SESSION_USER_KEY = "user"
 
 function login(email, password) {
     return apiService.writeToApi('/login', { email, password })
@@ -19,7 +20,10 @@ function logout() {
     // remove user from local storage to log user out
     localStorage.removeItem(SESSION_USER_KEY);
     sessionStorage.removeItem(SESSION_AUTH_KEY, '');
-    eventService.$emit("logged-off");
+}
+
+function currentUser() {
+    return JSON.parse(localStorage.getItem(SESSION_USER_KEY));
 }
 
 function handleLogin(response) {
@@ -30,15 +34,10 @@ function handleLogin(response) {
     }
 }
 
-function currentUser() {
-    return JSON.parse(localStorage.getItem(SESSION_USER_KEY));
-}
-
 function signIn(jwt) {
     sessionStorage.setItem(SESSION_AUTH_KEY, jwt);
 
     //destructure using IIFE
     var user = (({ first, last }) => ({ first, last }))(JWTDecode(jwt));
     localStorage.setItem(SESSION_USER_KEY, JSON.stringify(user))
-    eventService.$emit("logged-in", user);
 }
