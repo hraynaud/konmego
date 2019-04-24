72c2918f5cc1833586b06989f9c09aa5fe1e7135
Herby Raynaud <herby@yieldmo.com>
2019-04-24 00:00:07 -0400

    Adds compiled build files

---
+++ public/precache-manifest.229b07931d96e358c84cb69d08a0cd0c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "963a09cdcdef2c5ae7d3",
+    "url": "/js/main.acd674f1.js"
+  },
+  {
+    "revision": "c2f8c95100ce943892a0",
+    "url": "/js/chunk-vendors.5b49b470.js"
+  },
+  {
+    "revision": "c10b478c45d90f5ce5b363e9868eddd6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2f8c95100ce943892a0",
+    "url": "/css/chunk-vendors.f250d0d3.css"
+  }
+];
