da8ef364234fae8aea1ee0b5a121d0230b86b324
Herby Raynaud <herby@yieldmo.com>
2019-04-24 10:27:29 -0400

    fixes syntax error

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,6 @@ Rails.application.routes.draw do
   end
 
   match '*path', to: 'application#index', via: [:get], constraints: ->(request){
-    !request.xhr? && request.format.html
+    !request.xhr? && request.format.html?
   }
 end
