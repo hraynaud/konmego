0e1b124625447334f408fd68f7881d92bb6988e0
Herby Raynaud <herby@yieldmo.com>
2019-05-22 00:32:32 -0400

    wip projects api

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -3,7 +3,8 @@ class Api::V1::ProjectsController < ApplicationController
   before_action :authenticate_request
 
   def index
-    render json: {project: "blah"}
+    projects = Project.all
+    render json: projects.as_json 
   end
 end
 
