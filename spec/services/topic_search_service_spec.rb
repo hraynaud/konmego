a58d30f3749eb02ed41feb7c21cff1a8350fbac6
Herby Raynaud <herby@yieldmo.com>
2019-08-20 07:18:56 +0300

    moves relationships graph creation for specs into helper module and requires helper in specs

--- spec/services/topic_search_service_spec.rb
+++ spec/services/topic_search_service_spec.rb
@@ -1,13 +1,10 @@
 require 'set'
 require 'rails_helper'
+include RelationshipHelper
 
 describe TopicSearchService do
   before do
-    create_users
-    create_topics
-    setup_social_graph
-    create_endorsements
-    set_endorsement_statuses
+    setup_relationship_data
   end
 
   describe ".find_contacts_connected_to_topic_for" do
@@ -27,60 +24,6 @@ describe TopicSearchService do
       expect(TopicSearchService.find_contacts_connected_to_topic_for(@sar, "Singing", 2)).to eq []
     end
 
-  end
-
-  def create_users
-    @herby, @tisha, @franky, @fauzi, @kendra, @sar, @elsa, @vince, @jean = %w(
-      herby tisha franky fauzi kendra sar elsa vince jean
-    ).map do |fname|
-      FactoryBot.create(:person, first_name: fname.titleize, last_name: "Skillz")
-    end
-  end
-
-  def create_topics
-    @cooking, @fencing, @acting, @djing, @singing, @design, @composer = %w(
-      cooking fencing acting djing singing design composer
-    ).map do |skill|
-      FactoryBot.create(:topic, name: skill.titleize )
-    end
-  end
-
-  def setup_social_graph
-    RelationshipManager.befriend @herby, @tisha
-    RelationshipManager.befriend @tisha, @kendra
-    RelationshipManager.befriend @jean, @herby
-    RelationshipManager.befriend @fauzi, @herby
-    RelationshipManager.befriend @fauzi, @tisha
-    RelationshipManager.befriend @tisha, @vince
-    RelationshipManager.befriend @kendra, @vince
-    RelationshipManager.befriend @jean, @herby
-    RelationshipManager.befriend @herby,  @elsa 
-    RelationshipManager.befriend @franky,  @fauzi
-    RelationshipManager.befriend @sar, @elsa
-  end
-
-  def create_endorsements
-    @accepted = []
-    @pending = []
-    @declined = []
-
-    @accepted << EndorsementService.create_for_existing_person_node(@fauzi, @franky, @cooking)
-    @accepted << EndorsementService.create_for_existing_person_node(@tisha, @kendra, @singing)
-    @accepted << EndorsementService.create_for_existing_person_node(@tisha, @kendra, @cooking)
-
-    @declined << EndorsementService.create_for_existing_person_node(@vince, @jean, @composer)
-    @pending << EndorsementService.create_for_existing_person_node(@elsa, @sar, @acting)
-
-  end
-
-  def set_endorsement_statuses
-    @accepted.each do |e|
-      EndorsementService.accept(e)
-    end
-
-    @declined.each do |e|
-      EndorsementService.decline(e)
-    end
   end
 
 end
---
+++ spec/support/relationship_helper.rb
@@ -0,0 +1,64 @@
+module RelationshipHelper
+
+  def setup_relationship_data
+    create_users
+    create_topics
+    setup_social_graph
+    create_endorsements
+    set_endorsement_statuses
+  end
+
+  def create_users
+    @herby, @tisha, @franky, @fauzi, @kendra, @sar, @elsa, @vince, @jean = %w(
+      herby tisha franky fauzi kendra sar elsa vince jean
+    ).map do |fname|
+      FactoryBot.create(:person, first_name: fname.titleize, last_name: "Skillz")
+    end
+  end
+
+  def create_topics
+    @cooking, @fencing, @acting, @djing, @singing, @design, @composer = %w(
+      cooking fencing acting djing singing design composer
+    ).map do |skill|
+      FactoryBot.create(:topic, name: skill.titleize )
+    end
+  end
+
+  def setup_social_graph
+    RelationshipManager.befriend @herby, @tisha
+    RelationshipManager.befriend @tisha, @kendra
+    RelationshipManager.befriend @jean, @herby
+    RelationshipManager.befriend @fauzi, @herby
+    RelationshipManager.befriend @fauzi, @tisha
+    RelationshipManager.befriend @tisha, @vince
+    RelationshipManager.befriend @kendra, @vince
+    RelationshipManager.befriend @jean, @herby
+    RelationshipManager.befriend @herby,  @elsa 
+    RelationshipManager.befriend @franky,  @fauzi
+    RelationshipManager.befriend @sar, @elsa
+  end
+
+  def create_endorsements
+    @accepted = []
+    @pending = []
+    @declined = []
+
+    @accepted << EndorsementService.create_for_existing_person_node(@fauzi, @franky, @cooking)
+    @accepted << EndorsementService.create_for_existing_person_node(@tisha, @kendra, @singing)
+    @accepted << EndorsementService.create_for_existing_person_node(@tisha, @kendra, @cooking)
+
+    @declined << EndorsementService.create_for_existing_person_node(@vince, @jean, @composer)
+    @pending << EndorsementService.create_for_existing_person_node(@elsa, @sar, @acting)
+
+  end
+
+  def set_endorsement_statuses
+    @accepted.each do |e|
+      EndorsementService.accept(e)
+    end
+
+    @declined.each do |e|
+      EndorsementService.decline(e)
+    end
+  end
+end
