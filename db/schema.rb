12fc72b14eb5210da05ac2c1204196e7a43caadd
Herby Raynaud <herbyraynaud@ip-192-168-1-110.ec2.internal>
2020-02-29 14:16:08 -0500

    Removes Postgres related dated

--- db/schema.rb
+++ db/schema.rb
@@ -15,22 +15,4 @@ ActiveRecord::Schema.define(version: 2019_04_13_114844) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "projects", force: :cascade do |t|
-    t.string "name"
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_projects_on_user_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email", null: false
-    t.string "password_digest", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "first_name"
-    t.string "last_name"
-  end
-
-  add_foreign_key "projects", "users"
 end
