af5bbd1b11c24c23aaa69e9383df0c73285014a5
Herby Raynaud <herby.raynaud@gmail.com>
2020-04-19 18:03:37 -0400

    Adds localhost to cors allowed list

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
  Rails.application.config.middleware.insert_before 0, Rack::Cors do
    allow do
       #origins 'localhost:8080' #Add production domain here
-     origins { |source, env| Rails.env.development? && source == "http://localhost:8080"}
+     origins { |source, env| Rails.env.development? && source == "http://#{ENV['LOCAL_IP']}:8080"}
      resource '*',
        headers: :any,
        methods: [:get, :post, :put, :patch, :delete, :options, :head]
