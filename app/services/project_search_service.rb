25c863b51dd11c2010dc1667c0cd151b23d530aa
Herby Raynaud <herby.raynaud@gmail.com>
2020-08-02 16:41:14 -0400

    Refactor “static” methods into Eigen class

--- app/services/project_search_service.rb
+++ app/services/project_search_service.rb
@@ -1,36 +1,39 @@
 class ProjectSearchService
 
   DEFAULT_PROJECT_SEARCH_DEPTH = 1
+  class << self
 
-  def self.search user, params = {}
-    scope = initial_scope user, params
-    scope = by_topic(scope, params[:topic])
-    scope.pluck(:projects)
-  end
-
-
-  def self.initial_scope user, params
-    friend = are_first_friends? user, params[:friend]
-    friend ? projects_for(friend) : projects_for(all_contacts(user,params[:depth] || DEFAULT_PROJECT_SEARCH_DEPTH))
-  end
-
-  def self.are_first_friends? user, friend
-    user.friends_with?(friend) ? friend : nil
-  end
-
-  def self.all_contacts person, depth 
-    person.contacts(:contacts, :r, rel_length: 0..depth).distinct
-  end
-
-  def self.projects_for scope
-    scope.projects(:projects).where("projects.visibility > ? ", Project.visibilities[:private]).distinct 
-  end
-
-  def self.by_topic scope, topic_id
-    if topic_id
-      scope = scope.topic.where(id: topic_id)
+    def search user, params = {}
+      scope = initial_scope user, params
+      scope = by_topic(scope, params[:topic])
+      scope.pluck(:projects)
     end
-    scope
-  end
 
+    private
+
+    def initial_scope user, params
+      friend = are_first_friends? user, params[:friend]
+      friend ? projects_for(friend) : projects_for(all_contacts(user,params[:depth] || DEFAULT_PROJECT_SEARCH_DEPTH))
+    end
+
+    def are_first_friends? user, friend
+      user.friends_with?(friend) ? friend : nil
+    end
+
+    def all_contacts person, depth 
+      person.contacts(:contacts, :r, rel_length: 0..depth).distinct
+    end
+
+    def projects_for scope
+      scope.projects(:projects).where("projects.visibility > ? ", Project.visibilities[:private]).distinct 
+    end
+
+    def by_topic scope, topic_id
+      if topic_id
+        scope = scope.topic.where(id: topic_id)
+      end
+      scope
+    end
+
+  end
 end
