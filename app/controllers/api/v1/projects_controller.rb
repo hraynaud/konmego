2750ffa69bb4d93e7790861b6a7e8893b26801ce
Herby Raynaud <herby.raynaud@gmail.com>
2021-02-06 19:55:56 -0500

    Removes index action which used built serializer and no filtering
    Adds TODO comment

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -1,13 +1,18 @@
 class Api::V1::ProjectsController < ApplicationController
 
-  def index
-    projects = Project.all
-    render json: projects.as_json 
+  def create
+    current_user.projects.create project_params
   end
+
+  #TODO move consolidate search functionality in this controller or 
+  #projexct_search_controller
 
   def search
     render json: ProjectSearchService.all_by_topic(params[:topic])
   end
 
+  def project_params
+    params.require(:project).permit(:name, :description, :start_date, :end_date, obstacles: [  :description ] )
+  end
 end
 
