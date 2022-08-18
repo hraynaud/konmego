81f0af2fe588f98613f935815ca9929d831b105d
hraynaud <herby.raynaud@gmail.com>
2022-08-18 02:28:43 -0400

    adds custom header to allowed headers to be sent by server

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,9 +8,9 @@
  Rails.application.config.middleware.insert_before 0, Rack::Cors do
    allow do
       #origins 'localhost:8080' #Add production domain here
-     origins { |source, env| Rails.env.development? && source == "http://localhost:8081"}
+     origins { |source, env| Rails.env.development? && source == "http://localhost:9000"}
      resource '*',
        headers: :any,
-       methods: [:get, :post, :put, :patch, :delete, :options, :head], expose: ['Authorization,jwt']
+       methods: [:get, :post, :put, :patch, :delete, :options, :head], expose: ['Authorization,jwt','X-Message']
    end
  end
