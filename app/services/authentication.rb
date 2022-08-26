a55769a41b39bc9b223b342eeaa02d6d774ebce4
hraynaud <herby.raynaud@gmail.com>
2022-08-25 23:42:25 -0400

    moves methods around

--- app/services/authentication.rb
+++ app/services/authentication.rb
@@ -13,22 +13,27 @@ class Authentication
     end
   end
 
+  def self.login_success identity
+    Rails.logger.debug("login was successful")
+
+    OpenStruct.new({jwt: jwt_for(identity)})
+  end
+
   def self.uid_from_from_request_auth_hdr auth_hdr
     begin
-      header =  JWT.decode(auth_hdr, Rails.application.credentials.secret_key_base)
+      header =  JWT.decode(auth_hdr, Rails.application.secret_key_base, true, { algorithm: 'HS256' })
       header[0][USER_ID_KEY_PARAM]
+     
     rescue JWT::DecodeError
       raise  "Invalid Authorization Token Credentials"
     end
   end
 
-  def self.login_success identity
-    OpenStruct.new({jwt: jwt_for(identity)})
-  end
+
 
   def self.jwt_for identity 
     p = identity.person
-    JWT.encode({uid: identity.id, email: identity.email, name: p.name, exp: 1.day.from_now.to_i}, Rails.application.credentials.secret_key_base)
+    JWT.encode({uid: identity.id, email: identity.email, name: p.name, exp: 1.day.from_now.to_i}, Rails.application.secret_key_base,'HS256')
   end
 
 end
