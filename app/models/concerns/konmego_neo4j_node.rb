228ba7d2c3bdcb986c9130e8030503c50011fb1d
hraynaud <herby.raynaud@gmail.com>
2022-09-02 17:44:11 -0400

    Redefines first and last methods to use created_at time

--- app/models/concerns/konmego_neo4j_node.rb
+++ app/models/concerns/konmego_neo4j_node.rb
@@ -6,6 +6,9 @@ module KonmegoNeo4jNode
     include ActiveGraph::Node
     include ActiveGraph::Timestamps::Created
     #include ActiveGraph::Timestamps::Updated NOTE this causes weird test failures
+    scope :by_created, -> { order(created_at: :asc) }
+    scope :first, -> { by_created.first }
+    scope :last, -> { by_created.last }
 
     def type
       self.class.name
