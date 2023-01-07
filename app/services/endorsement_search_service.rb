4e23ab7673569bf2e0eb2fbbc69d0c8dbb7ef217
hraynaud <herby.raynaud@gmail.com>
2023-01-06 19:41:37 -0500

    rubo-cop

--- app/services/endorsement_search_service.rb
+++ app/services/endorsement_search_service.rb
@@ -8,63 +8,59 @@ class EndorsementSearchService
       transform(current_user, graph)
     end
 
-
     private
 
-    def get_endorsement_graph current_user, topic, hops
+    def get_endorsement_graph(current_user, topic, hops)
       current_user.query_as(:u)
-        .with(:u)
-        .match(match_query(hops))
-        .params(topic_name: topic)
-        .return('relationships(p) as r_knows', 'nodes(p) as full_path', :r_src, :r_topic, :t, :e, :endorser, :endorsee)
+                  .with(:u)
+                  .match(match_query(hops))
+                  .params(topic_name: topic)
+                  .return('relationships(p) as r_knows', 'nodes(p) as full_path', :r_src, :r_topic, :t, :e, :endorser, :endorsee)
     end
 
-    def transform person, data
+    def transform(person, data)
       topic_paths = extract_paths(person, data)
-       topic_paths.map do |topic_path|
-         topic_path
+      topic_paths.map do |topic_path|
+        topic_path
       end
     end
 
     def openstruct_to_hash(object, hash = {})
       case object
-      when OpenStruct then
+      when OpenStruct
         object.each_pair do |key, value|
           hash[key] = openstruct_to_hash(value)
         end
         hash
-      when Array then
+      when Array
         object.map { |v| openstruct_to_hash(v) }
       else object
       end
     end
 
-    def extract_paths person, data
+    def extract_paths(person, data)
       data.map do |record|
-        @extractor = ::PathExtractor.new(person,record)
+        @extractor = ::PathExtractor.new(person, record)
         openstruct_to_hash(OpenStruct.new(path: @extractor.path, endorsement: @extractor.obfuscated_endorsement))
       end
     end
 
-
-
-    #TODO FIXME 
-    #1. Figure out how to use parameters in the relationship length clause
-    #so that we don't have to string build (i.e. #{hops})
-    #2. Is it possible to build this path query using the neo4j.rb ActiveNode DSL?
+    # TODO: FIXME
+    # 1. Figure out how to use parameters in the relationship length clause
+    # so that we don't have to string build (i.e. #{hops})
+    # 2. Is it possible to build this path query using the neo4j.rb ActiveNode DSL?
     #
-    def match_query hops
+    def match_query(hops)
       <<-CYPHER
- p = (u)-[:`KNOWS`*0..#{hops}]-(endorser:`Person`) 
+ p = (u)-[:`KNOWS`*0..#{hops}]-(endorser:`Person`)#{' '}
  WITH *
  MATCH (endorser)<-[r_src:`ENDORSEMENT_SOURCE`]-(e:`Endorsement`) WHERE (e.status = 1)
- MATCH (e)-[r_target:`ENDORSEMENT_TARGET`]->(endorsee:`Person`) 
+ MATCH (e)-[r_target:`ENDORSEMENT_TARGET`]->(endorsee:`Person`)#{' '}
  MATCH (e)-[r_topic:`TOPIC`]->(t:`Topic`) WHERE (t.name = {topic_name})
  WITH *
   WHERE ALL(x IN NODES(p) WHERE SINGLE(y IN NODES(p) WHERE y = x))
       CYPHER
     end
-
 
     # line1: find all paths people current user(u) "KNOWS"  within number of hops
     # line2: gather all paths
@@ -72,23 +68,19 @@ class EndorsementSearchService
     # line4  find all endorsees within the list of people u knows)
     # line5: find all endorsements in the list where endorsemeent topic matches topic
     # line6: gather all all results
-    # line7: find 
+    # line7: find
 
-
-
-    #Attempt fo refactor original
-    def match_query2 hops
+    # Attempt fo refactor original
+    def match_query2(hops)
       <<-CYPHER
- p = (u)-[:`KNOWS`*0..#{hops}]-(endorser:`Person`) 
+ p = (u)-[:`KNOWS`*0..#{hops}]-(endorser:`Person`)#{' '}
  WITH *
  MATCH (e:`Endorsement`)-[r_src:`ENDORSEMENT_SOURCE`]->(endorser) WHERE (e.status = 1)
- MATCH (e)-[r_target:`ENDORSEMENT_TARGET`]->(endorsee:`Person`) 
+ MATCH (e)-[r_target:`ENDORSEMENT_TARGET`]->(endorsee:`Person`)#{' '}
  MATCH (e)-[r_topic:`TOPIC`]->(t:`Topic`) WHERE (t.name = {topic_name})
  WITH *
   WHERE ALL(x IN NODES(p) WHERE SINGLE(y IN NODES(p) WHERE y = x))
       CYPHER
     end
-
-
   end
 end
