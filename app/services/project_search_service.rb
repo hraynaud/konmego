2ba04599ff3685b001a2c85a1693abc83be5cc84
hraynaud <herby.raynaud@gmail.com>
2024-01-15 22:25:30 -0500

    rubocop linting

--- app/services/project_search_service.rb
+++ app/services/project_search_service.rb
@@ -1,9 +1,7 @@
 class ProjectSearchService
-
   DEFAULT_PROJECT_SEARCH_DEPTH = 1
   class << self
-
-    def search user, params = {}
+    def search(user, params = {})
       scope = initial_scope user, params
       scope = by_topic(scope, params[:topic])
       scope.pluck(:projects)
@@ -23,26 +21,23 @@ class ProjectSearchService
       friend_id ? Person.find(friend_id) : nil
     end
 
-    def initial_scope user, params
+    def initial_scope(user, params)
       depth = params[:depth] || DEFAULT_PROJECT_SEARCH_DEPTH
       friend = are_first_friends? user, resolve_friend(params[:friend])
-      friend ? projects_for(friend) : projects_for(user.contacts_by_depth(depth))
+      projects_for(friend || user.contacts_by_depth(depth))
     end
 
-    def are_first_friends? user, friend
+    def are_first_friends?(user, friend)
       user.friends_with?(friend) ? friend : nil
     end
 
-    def projects_for scope
-      scope.projects(:projects).public.distinct 
+    def projects_for(scope)
+      scope.projects(:projects).public.distinct
     end
 
-    def by_topic scope, topic_id
-      if topic_id
-        scope = scope.topic.where(id: topic_id)
-      end
+    def by_topic(scope, topic_id)
+      scope = scope.topic.where(id: topic_id) if topic_id
       scope
     end
-
   end
 end
