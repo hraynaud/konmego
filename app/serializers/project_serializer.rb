7e7caa46b09af861d63110a9a75eb9c2890e62a1
hraynaud <herby.raynaud@gmail.com>
2024-01-15 22:25:49 -0500

    adds owner_avatar_url

--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -1,6 +1,9 @@
 class ProjectSerializer
   include JSONAPI::Serializer
   set_key_transform :camel_lower
-  attributes :name, :description, :status, :topic_name,:topic_image, :hero_image_url
+  attributes :name, :description, :status, :topic_name, :topic_image, :hero_image_url
+  attribute :owner_avatar_url do |o, _params|
+    o.owner.avatar_url
+  end
   attribute :obstacles
 end
