f3a6df6d002d819bca4a4052cf8b61f00a6a27d7
hraynaud <herby.raynaud@gmail.com>
2024-01-21 21:24:49 -0500

    rubocop

--- app/services/endorsement_service.rb
+++ app/services/endorsement_service.rb
@@ -16,7 +16,7 @@ class EndorsementService
 
       endorsement = create_from_nodes(endorser, endorsee, topic_name)
 
-      if(endorsee.status == "non_member") #TODO replace with enum
+      if endorsee.status == 'non_member' # TODO: replace with enum
         EndorsementMailer.with(id: endorsement.id).non_member_email.deliver_later
       else
         EndorsementMailer.with(id: endorsement.id).member_email.deliver_later
@@ -25,40 +25,43 @@ class EndorsementService
     end
 
     def accept(endorsement, user)
-      raise StandardError, "Invalid Operation" if endorsement.to_node != user
+      raise StandardError, 'Invalid Operation' if endorsement.to_node != user
+
       endorsement.accept!
       RelationshipManager.create_friendship_if_none_exists_for(endorsement)
       endorsement
     end
 
     def decline(endorsement, user)
-      raise StandardError, "Invalid Operation" if endorsement.to_node != user
+      raise StandardError, 'Invalid Operation' if endorsement.to_node != user
+
       endorsement.decline!
       endorsement
     end
 
-    def destroy endorsement, user
-      raise StandardError, "Invalid Operation" unless can_destroy? endorsement, user
+    def destroy(endorsement, user)
+      raise StandardError, 'Invalid Operation' unless can_destroy? endorsement, user
+
       endorsement.destroy
     end
 
     def find(id)
       endorser_id, endorsee_id, topic = decompose_id(id)
-      from = PersonService.find_by_id(endorser_id)  
-      from.endorsements.each_rel.select{|r|r.to_node.id == endorsee_id && r.topic == topic}.first
+      from = PersonService.find_by_id(endorser_id)
+      from.endorsements.each_rel.select { |r| r.to_node.id == endorsee_id && r.topic == topic }.first
     end
 
-    def generate_id endorser_id, endorsee_id, topic
+    def generate_id(endorser_id, endorsee_id, topic)
       from_id = Obfuscation::IdCodec.encode(endorser_id)
-      to_id =  Obfuscation::IdCodec.encode(endorsee_id)
-      encoded= "#{from_id}:#{to_id}:#{topic}"
+      to_id = Obfuscation::IdCodec.encode(endorsee_id)
+      encoded = "#{from_id}:#{to_id}:#{topic}"
       Obfuscation::IdCodec.urlsafe_encode64(encoded)
     end
 
-    def decompose_id encoded_id
-      _unbase64 = Obfuscation::IdCodec.urlsafe_decode64(encoded_id)
-      _from, _to, topic = _unbase64.split(":")
-      return [Obfuscation::IdCodec.decode(_from), Obfuscation::IdCodec.decode(_to),topic]
+    def decompose_id(encoded_id)
+      unbase64 = Obfuscation::IdCodec.urlsafe_decode64(encoded_id)
+      from, to, topic = unbase64.split(':')
+      [Obfuscation::IdCodec.decode(from), Obfuscation::IdCodec.decode(to), topic]
     end
 
     # def by_status(user, status)
@@ -78,38 +81,39 @@ class EndorsementService
 
     private
 
-    def can_destroy? endorsement, user
-      status_accepted_and_user_is_either_party?(endorsement, user) || status_pending_or_declined_and_user_is_endorser?(endorsement, user)
+    def can_destroy?(endorsement, user)
+      status_accepted_and_user_is_either_party?(endorsement,
+                                                user) || status_pending_or_declined_and_user_is_endorser?(endorsement,
+                                                                                                          user)
     end
 
-    def status_accepted_and_user_is_either_party? endorsement, user
+    def status_accepted_and_user_is_either_party?(endorsement, user)
       endorsement.status == Endorse.accepted && (endorsement.to_node == user || endorsement.from_node == user)
     end
 
-    def status_pending_or_declined_and_user_is_endorser? endorsement, user
+    def status_pending_or_declined_and_user_is_endorser?(endorsement, user)
       (endorsement.status == Endorse.pending || endorsement.status == Endorse.declined) && endorsement.from_node == user
     end
 
     def find_or_create_endorsee(params)
       endorsee = Person.where(id: params[:endorsee_id]).first
-      if endorsee
-        return endorsee
-      else
-        unless params[:email] && params[:first_name] && params[:last_name]
-          raise raise StandardError, "Can't create and invite endorsee; email, first_name, last_name are required."
-        end
-        
-        endorsee = PersonService.create_by_endorserment(params)
-      
-        endorsee.status = 'non_member'
+      return endorsee if endorsee
+
+      unless params[:email] && params[:first_name] && params[:last_name]
+        raise raise StandardError, "Can't create and invite endorsee; email, first_name, last_name are required."
       end
+
+      endorsee = PersonService.create_by_endorserment(params)
+
+      endorsee.status = 'non_member'
+
       endorsee
     end
 
-    def validate_non_duplicated_endorsement endorser, endorsee, topic_name
-      if already_exists?(endorser, endorsee, topic_name)
-        raise raise StandardError, "You've already endorsed #{endorsee.name} for #{topic_name}"
-      end
+    def validate_non_duplicated_endorsement(endorser, endorsee, topic_name)
+      return unless already_exists?(endorser, endorsee, topic_name)
+
+      raise raise StandardError, "You've already endorsed #{endorsee.name} for #{topic_name}"
     end
 
     def set_topic(params)
