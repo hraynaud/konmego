664d47b04ad5a0e574f27199d039d8fd7947d137
hraynaud <herby.raynaud@gmail.com>
2025-01-11 10:50:17 -0500

    set log level

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,14 +34,14 @@ Rails.application.configure do
   config.active_support.deprecation = :log
 
   # Raise an error on page load if there are pending migrations.
-  #config.active_record.migration_error = :page_load
+  # config.active_record.migration_error = :page_load
 
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = false
   config.neo4j.verbose_query_logs = false
-  config.neo4j.logger nil
+  config.neo4j.logger = Rails.logger
+  config.neo4j.pretty_logged_cypher_queries
   config.log_level = :info
-
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
@@ -49,7 +49,6 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-
 
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
