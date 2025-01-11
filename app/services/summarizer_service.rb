bacd4ec563272987a04d40c53f87aae37792e0f3
hraynaud <herby.raynaud@gmail.com>
2025-01-10 22:16:27 -0500

    adds topic and summary services

---
+++ app/services/summarizer_service.rb
@@ -0,0 +1,61 @@
+class SummarizerService
+  SEARCH_PROMPT = %(
+You are an expert in semantic search and skilled at analyzing text for the purpose of generating concise,
+structured summaries and keyword lists for indexing. When given a short passage that is either a biographical
+profile or testimonial describing skills, expertise, or activities, you will perform the following:
+
+1. **Generate a summary**:
+Analyze the passage and produce a concise summary of no more than **two sentences**.
+Focus on identifying key skills, expertise, or activities. Removing any rhetorical embellishments, subjective descriptions
+and or unrelated personal information. The summary should be neutral, factual, and precise.
+
+2. **Create a list of keywords**:
+ - Extract synonyms and related terms that directly describe the key skills or activities.
+ - Include conceptually related terms, even if not explicitly mentioned, based on logical connections or common practice
+  (e.g., "yoga" → "pilates," "martial arts" → "self-defense").
+
+### Example:
+**Input**:
+"My name is Amy. I live in Melbourne, Australia. I'm a civil engineer who is passionate about sustainable urban development.
+ I enjoy pottery and jazz music in my free time and go to yoga class muliple times a week.  Former youth karate champion."
+
+**Output**:
+
+**Summary**:
+Amy is a civil engineer from Melbourne, Australia, with expertise in sustainable urban development.
+She is also skilled in karate and has interests in pottery and jazz music and yoga.
+
+**Keywords**:
+- Civil engineering
+- Sustainable urban development
+- Urban planning
+- Urban design
+- City planning
+- Jazz music
+- Music
+- Karate
+- Martial arts
+- Combat sports
+- Pottery
+- Ceramics
+- Crafts
+- Yoga
+- Pilates
+- Stretching
+- Flexibility
+- Mindfulness
+- Meditation
+---
+ Here is the text:
+
+
+    ).freeze
+
+  class << self
+    def search(search)
+      search_prompt = "#{SEARCH_PROMPT} \n __\n ### #{search} \n"
+      completion = OllamaService.completion(search_prompt)
+      OllamaService.parse_completion completion
+    end
+  end
+end
---
+++ app/services/topic_suggestion_service.rb
@@ -0,0 +1,80 @@
+module TopicSuggestionService
+  class << self
+    SEARCH_PROMPT = %(
+    You are powerful semantic search and thesaurus robot.
+    You reply in JSON format with the field 'topic'.
+    Given some text you will identify the key
+    skills, talents, and competencies related to the text.
+
+    Based on your thorough understanding of the text select the topic from the following list the best matches
+    the knowledge domain mentioned in the text. Please sumit your answer in the follow JSON format: {"topic": "TOPIC"}
+    You reply with  exactly one word response which is best matching topic from the list.
+    here is the topic list you must choose from:
+    ["Animation",
+      Architecture",
+      Berlin",
+      Birdwatching",
+      Charleston",
+      Chicago",
+      Child Healthcare",
+      Climate Change",
+      Copenhagen",
+      Corporate Training",
+      Craft Beer Brewing",
+      Cycling",
+      Design",
+      E-Commerce",
+      Engineering",
+      Fashion",
+      Fashion Design",
+      Fintech",
+      Gardening",
+      Golf",
+      Graphic Design",
+      Heart Disease",
+      Hiking",
+      History",
+      Jazz Music",
+      Journalism",
+      Medicine",
+      New York",
+      Painting",
+      Photography",
+      Piano",
+      Portland",
+      Pottery",
+      Rock Climbing",
+      Sailing",
+      Salsa Dancing",
+      San Diego",
+      Seattle",
+      Software Engineering",
+      Sustainable Fashion",
+      Urban Design",
+      Urban Sustainability",
+      User Experience",
+      Washington D.C.",
+      Yoga"
+      ]
+
+      Here is the search text:
+
+
+    ).freeze
+
+    def suggest(search_qry)
+      return nil unless search_qry
+
+      search_prompt = build_search_prompt(search_qry)
+      completion = OllamaService.completion(search_prompt)
+      data = OllamaService.parse_completion(completion)
+      topic = data[:topic]
+      Rails.logger.debug('Topic is ${topic}')
+      topic
+    end
+
+    def build_search_prompt(search)
+      "#{SEARCH_PROMPT} \n __\n #{search} \n"
+    end
+  end
+end
