969836849ddb8acf8783ef455477c8e0578834fe
hraynaud <herby.raynaud@gmail.com>
2025-07-27 20:04:15 -0400

    linting errors

--- rails-app/spec/support/test_utils.rb
+++ rails-app/spec/support/test_utils.rb
@@ -1,46 +1,44 @@
 module TestUtils
   def extract_errors
-    response.headers["X-Message"]
+    response.headers['X-Message']
   end
 
-  def parse_body response
+  def parse_body(response)
     JSON.parse(response.body)
   end
 
-  def do_put user, path, payload={}
-    put path, params: payload, headers: auth_header(user) 
+  def do_put(user, path, payload = {})
+    put path, params: payload, headers: auth_header(user)
   end
 
-  def do_post user, path, payload={}
-    post path, params: payload, headers: auth_header(user) 
+  def do_post(user, path, payload = {})
+    post path, params: payload, headers: auth_header(user)
   end
 
-  def do_get user, path
+  def do_get(user, path)
     get path, headers: auth_header(user)
   end
 
-  def auth_header user 
-    {'Authorization': Authentication.jwt_for(user)}
+  def auth_header(user)
+    { Authorization: Authentication.jwt_for(user) }
   end
 
-  def i18n_attributes_error err_key, opts = {}
+  def i18n_attributes_error(err_key, opts = {})
     /#{I18n.t("errors.attributes.#{err_key}", **opts)}/
   end
 
-  #method to map random simple to actually enums on model returns nil if key not in enum_options 
-  def match_enum_key_from_options enum_options, key
+  # method to map random simple to actually enums on model returns nil if key not in enum_options
+  def match_enum_key_from_options(enum_options, key)
     enum_options.key(enum_options[key])
   end
 
   module RSpec
-
-    def expect_response_and_model_json_to_match response, model
-      expect(parse_body(response)).to eq (JSON.parse(model.to_json))
+    def expect_response_and_model_json_to_match(response, model)
+      expect(parse_body(response)).to eq(JSON.parse(model.to_json))
     end
 
-    def expect_http response, status
+    def expect_http(response, status)
       expect(response).to have_http_status(status)
     end
-
   end
 end
