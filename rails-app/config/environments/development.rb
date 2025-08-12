8feeef18b27f7526bc4c756fbb21e52521417ce4
hraynaud <herby.raynaud@gmail.com>
2025-08-12 16:50:10 -0400

    cleans up docker compose for rails allows all host inbound in dev

--- rails-app/config/environments/development.rb
+++ rails-app/config/environments/development.rb
@@ -52,7 +52,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
-  config.hosts << 'localhost'
-  config.hosts << 'konmego-server.fly.dev'
+  config.hosts.clear
+
 
 end
